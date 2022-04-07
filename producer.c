4190d16e27a75677a0512efc6a75978b0cfec730
Brandon Cossin <72531057+brandoncossin@users.noreply.github.com>
2022-04-07 13:56:05 -0400

    Update producer.c

--- producer.c
+++ producer.c
@@ -9,9 +9,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define SEM_NAME "/semaphore_counter"
 #define SHM_NAME "/shared_table"
+
 int main(){
   //shared memory creation
   struct table_name {
@@ -19,6 +22,7 @@ int main(){
   };
   void *addr;
   int size = 4096;
+  //int sd = shm_unlink(SHM_NAME);
   int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
   struct table_name *tble = mmap(NULL, size, PROT_WRITE, MAP_SHARED, fd, 0);
   //semaphore logic
@@ -32,6 +36,7 @@ int main(){
   sem_post(semaphore);
   while(LOOP_TIMES > 0){
     sem_wait(semaphore);
+    //sleep(1);
     if(tble->buf[0] == 0 & tble->buf[1] == 0){
       int tmp1 = (rand() % 9) + 1;
       tble->buf[0] = tmp1;
@@ -42,9 +47,10 @@ int main(){
       printf("\n");
       --LOOP_TIMES;
     }
-    //const char num[50] = "44";
-    //memcpy(addr, num, strlen(num)+1);
     sem_post(semaphore);
   }
+  sem_close(semaphore);
+  sem_unlink(SEM_NAME);
+  shm_unlink(SHM_NAME);
   return 0;
 }
